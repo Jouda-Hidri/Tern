0b08e52f5cb7d73eadbd53684e18ad809f1a52e6
Jouda Hidri <hidrijouda@gmail.com>
2023-07-02 22:31:36 +0200

    Implement async for save

--- src/main/kotlin/tern/artic/ArticService.kt
+++ src/main/kotlin/tern/artic/ArticService.kt
@@ -3,11 +3,13 @@ package tern.artic
 import com.google.protobuf.Empty
 import io.grpc.ManagedChannel
 import io.grpc.ManagedChannelBuilder
+import io.grpc.stub.StreamObserver
 import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Service
 import tern.antarctic.Message
 import tern.grpc.TernServiceGrpc
 import tern.grpc.TernServiceOuterClass.SaveRequest
+import tern.grpc.TernServiceOuterClass.SaveResponse
 
 @Service
 class ArticService() {
@@ -15,12 +17,12 @@ class ArticService() {
     private var channel: ManagedChannel = ManagedChannelBuilder.forTarget("antarctic.default.svc.cluster.local:30000")
         .usePlaintext()
         .build()
-    private var stub = TernServiceGrpc.newBlockingStub(channel)
+    private var blockingStub = TernServiceGrpc.newBlockingStub(channel)
+    private var stub = TernServiceGrpc.newStub(channel)
 
     fun find(): List<Message> {
-        logger.info("antarctic.default.svc.cluster.local:30000")
         logger.info("Artic - Retrieving messages")
-        val response = stub.getMessage(Empty.getDefaultInstance())
+        val response = blockingStub.getMessage(Empty.getDefaultInstance())
         val list: MutableList<Message> = mutableListOf()
         response.forEach { getResponse ->
             list.add(Message(id = null, text = getResponse.text))
@@ -31,9 +33,21 @@ class ArticService() {
     fun save(message: Message) {
         logger.info("Artic - Request message: $message")
         stub.saveMessage(
-            SaveRequest.newBuilder()
-                .setText(message.text)
-                .build()
+            SaveRequest.newBuilder().setText(message.text).build(), object: StreamObserver<SaveResponse> {
+                override fun onNext(response: SaveResponse?) {
+                    // todo return response
+                    logger.warn("Artic - next $response")
+                }
+
+                override fun onError(throwable: Throwable?) {
+                    logger.error("Artic - Error")
+                }
+
+                override fun onCompleted() {
+                    // todo webClient call here ;)
+                    logger.info("Artic - Completed")
+                }
+            }
         )
     }
 }
