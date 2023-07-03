bb1f2486532cb93389aa844398b5a2623eae920c
Jouda Hidri <hidrijouda@gmail.com>
2023-07-03 16:48:49 +0200

    Log Tapi response line by line

--- src/main/kotlin/tern/artic/ArticService.kt
+++ src/main/kotlin/tern/artic/ArticService.kt
@@ -5,12 +5,23 @@ import io.grpc.ManagedChannel
 import io.grpc.ManagedChannelBuilder
 import io.grpc.stub.StreamObserver
 import org.slf4j.LoggerFactory
+import org.springframework.core.io.buffer.DataBuffer
+import org.springframework.core.io.buffer.DataBufferUtils
 import org.springframework.stereotype.Service
 import org.springframework.web.reactive.function.client.WebClient
 import tern.antarctic.Message
 import tern.grpc.TernServiceGrpc
 import tern.grpc.TernServiceOuterClass.SaveRequest
 import tern.grpc.TernServiceOuterClass.SaveResponse
+import java.io.BufferedReader
+import java.io.ByteArrayInputStream
+import java.io.ByteArrayOutputStream
+import java.io.InputStreamReader
+import java.nio.charset.StandardCharsets
+import java.nio.file.Files
+import java.nio.file.Path
+import java.nio.file.Paths
+
 
 @Service
 class ArticService() {
@@ -35,7 +46,7 @@ class ArticService() {
     fun save(message: Message) {
         logger.info("Artic - Request message: $message")
         stub.saveMessage(
-            SaveRequest.newBuilder().setText(message.text).build(), object: StreamObserver<SaveResponse> {
+            SaveRequest.newBuilder().setText(message.text).build(), object : StreamObserver<SaveResponse> {
                 override fun onNext(response: SaveResponse?) {
                     // todo possible to use response as path param
                     logger.warn("Artic - $response")
@@ -46,15 +57,41 @@ class ArticService() {
                 }
 
                 override fun onCompleted() {
-                    val status = client.get()
-                        .uri("/")
-                        .exchangeToMono { it.toEntity(Void::class.java) }
-                        .map { it.statusCode }
-                        .block()
-                        ?.value()
-                    logger.info("Artic - status $status")
+                    logger.info("Artic - Completed")
+                    val flux = client.get().retrieve().bodyToFlux(DataBuffer::class.java)
+
+                    val outputStream = ByteArrayOutputStream()
+                    val dataBufferOutputStream = DataBufferUtils.write(flux, outputStream)
+
+                    dataBufferOutputStream.blockLast()
+                    val byteBuffer = outputStream.toByteArray()
+
+                    val inputStream = ByteArrayInputStream(byteBuffer)
+                    val reader = BufferedReader(InputStreamReader(inputStream))
+
+                    try {
+                        while (true) {
+                            val line = reader.readLine() ?: break
+                            logger.info("Tapi - $line")
+                        }
+                    } finally {
+                        reader.close()
+                        inputStream.close()
+                    }
+
+                    logger.info("Tapi - Download completed.")
+
                 }
             }
         )
+    }
+
+    fun status(): Int? {
+        return client.get()
+            .uri("/")
+            .exchangeToMono { it.toEntity(Void::class.java) }
+            .map { it.statusCode }
+            .block()
+            ?.value()
     }
 }
