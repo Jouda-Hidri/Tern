3d16a5133e827e0a7bf77249765d3ef40e579e92
Jouda Hidri <joudahidri@Yiotiss-MacBook-Air.local>
2023-07-03 17:14:39 +0200

    Run both services in the cluster, and stream the Tapi CSV
    Antarctic becomes a ClusterIP service that Artic resolves by name, so the
    same image runs twice in the two roles. Saving is asynchronous, and on the
    gRPC callback Artic pulls a very large CSV from Tapi with WebClient -
    streamed rather than buffered, which is the whole reason it is there.

--- README.md
+++ README.md
@@ -1,16 +1,68 @@
 # Tern
 
-./mvnw clean package    
-docker-compose up --build
+The Arctic tern holds the record for the longest migration route of any bird, traveling from the Arctic to the Antarctic and back again every year.
 
-http://localhost:8080/h2-console/
+The Antarctic tern is a species of tern that is native to the Antarctic region.
 
-curl -d '{"text":"jouda"}' -H "Content-Type: application/json" -X POST http://localhost:8080    
-curl http://localhost:8080/
+We want to migrate a service (Tern service) from legacy (Antarctic version) to a new version (Arctic version). We want Arctic to be receiving the traffic and synch with Antarctic, without falling in the issue of cascading HTTP.
+
+We want to use Istio to maintain 2 deployments of the same app. The 2 services communicate using gRPC. On the gRPC callback, we make a call to Tapi using WebClient. Tapi exposes a a very large CSV file that we want to read using streaming, to avoid the issue of loading a large data in memory.
+
+## Setup
+
+Make sure you have Docker, Minikube and maven 3.6.3 installed.    
+
+````
+minikube start #(using virtualbox)    
+minikube addons enable metrics-server    
+eval $(minikube docker-env)    
+mvn clean install    
+docker build -t tern .    
+cd deployment    
+kubectl apply -f artic.yaml    
+kubectl apply -f antartic.yaml    
+kubectl apply -f postgres.yaml
+minikube dashboard
+````
+
+```minikube service artic```    
+
+````
+curl -d '{"text":"some-text"}' -H "Content-Type: application/json" -X POST {artic_host}    
+curl {artic_host}
+````
+
+## Istio
+Make sure you have istio 1.18 installed.    
+
+````
+# to download istio:    
+curl -L https://istio.io/downloadIstio | sh -
+# from inside istio folder
+export PATH=$PWD/bin:$PATH
+istioctl install --set profile=demo -y
+
+kubectl label namespace default istio-injection=enabled    
+
+kubectl apply -f https://raw.githubusercontent.com/istio/istio/release-1.18/samples/addons/prometheus.yaml    
+kubectl apply -f https://raw.githubusercontent.com/istio/istio/release-1.18/samples/addons/kiali.yaml    
+kubectl apply -f https://raw.githubusercontent.com/istio/istio/release-1.18/samples/addons/grafana.yaml    
+
+istioctl dashboard kiali
+````
+
 
 ## Locust
-locust    
-http://0.0.0.0:8089/
+In order to have a loadTest and see traffic animation on Kiali
 
-Host: http://localhost:8080
+<img width="766" alt="Screenshot 2023-07-02 at 16 01 47" src="https://github.com/Jouda-Hidri/Tern/assets/30729085/f7c67457-2a28-4841-9a17-edfa6f826a08">
+
+To setup Locust, clone this project https://github.com/Jouda-Hidri/tern-lt
+
+## Tapi
+On the gRPC callback, Tapi is called
+<img width="768" alt="Screenshot 2023-07-03 at 13 12 50" src="https://github.com/Jouda-Hidri/Tern/assets/30729085/17763716-9c9e-4247-9e4b-70ad0819b54b">
+
+To setup Tapi, clone this project: https://github.com/Jouda-Hidri/tapi
+
 
--- src/main/kotlin/tern/artic/ArticService.kt
+++ src/main/kotlin/tern/artic/ArticService.kt
@@ -3,23 +3,39 @@ package tern.artic
 import com.google.protobuf.Empty
 import io.grpc.ManagedChannel
 import io.grpc.ManagedChannelBuilder
+import io.grpc.stub.StreamObserver
 import org.slf4j.LoggerFactory
+import org.springframework.core.io.buffer.DataBuffer
+import org.springframework.core.io.buffer.DataBufferUtils
 import org.springframework.stereotype.Service
+import org.springframework.web.reactive.function.client.WebClient
 import tern.antarctic.Message
 import tern.grpc.TernServiceGrpc
 import tern.grpc.TernServiceOuterClass.SaveRequest
+import tern.grpc.TernServiceOuterClass.SaveResponse
+import java.io.BufferedReader
+import java.io.ByteArrayInputStream
+import java.io.ByteArrayOutputStream
+import java.io.InputStreamReader
+import java.nio.charset.StandardCharsets
+import java.nio.file.Files
+import java.nio.file.Path
+import java.nio.file.Paths
+
 
 @Service
 class ArticService() {
     private val logger = LoggerFactory.getLogger(ArticService::class.java)
-    private var channel: ManagedChannel = ManagedChannelBuilder.forAddress("localhost", 9090)
+    private var channel: ManagedChannel = ManagedChannelBuilder.forTarget("antarctic.default.svc.cluster.local:30000")
         .usePlaintext()
         .build()
-    private var stub = TernServiceGrpc.newBlockingStub(channel)
+    private var blockingStub = TernServiceGrpc.newBlockingStub(channel)
+    private var stub = TernServiceGrpc.newStub(channel)
+    var client: WebClient = WebClient.create("http://tapi.default.svc.cluster.local:5000")
 
     fun find(): List<Message> {
         logger.info("Artic - Retrieving messages")
-        val response = stub.getMessage(Empty.getDefaultInstance())
+        val response = blockingStub.getMessage(Empty.getDefaultInstance())
         val list: MutableList<Message> = mutableListOf()
         response.forEach { getResponse ->
             list.add(Message(id = null, text = getResponse.text))
@@ -30,9 +46,52 @@ class ArticService() {
     fun save(message: Message) {
         logger.info("Artic - Request message: $message")
         stub.saveMessage(
-            SaveRequest.newBuilder()
-                .setText(message.text)
-                .build()
+            SaveRequest.newBuilder().setText(message.text).build(), object : StreamObserver<SaveResponse> {
+                override fun onNext(response: SaveResponse?) {
+                    // todo possible to use response as path param
+                    logger.warn("Artic - $response")
+                }
+
+                override fun onError(throwable: Throwable?) {
+                    logger.error("Artic - Error ${throwable?.message}")
+                }
+
+                override fun onCompleted() {
+                    logger.info("Artic - Completed")
+                    val flux = client.get().retrieve().bodyToFlux(DataBuffer::class.java)
+
+                    val outputStream = ByteArrayOutputStream()
+                    val dataBufferOutputStream = DataBufferUtils.write(flux, outputStream)
+
+                    dataBufferOutputStream.blockLast()
+                    val byteBuffer = outputStream.toByteArray()
+
+                    val inputStream = ByteArrayInputStream(byteBuffer)
+                    val reader = BufferedReader(InputStreamReader(inputStream))
+
+                    try {
+                        while (true) {
+                            val line = reader.readLine() ?: break
+                            logger.info("Tapi - $line")
+                        }
+                    } finally {
+                        reader.close()
+                        inputStream.close()
+                    }
+
+                    logger.info("Tapi - Download completed.")
+
+                }
+            }
         )
+    }
+
+    fun status(): Int? {
+        return client.get()
+            .uri("/")
+            .exchangeToMono { it.toEntity(Void::class.java) }
+            .map { it.statusCode }
+            .block()
+            ?.value()
     }
 }
