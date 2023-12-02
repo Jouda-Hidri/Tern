99aabdc9e41a5f3ec6d96b662ab8167e12b994da
Jouda Hidri <hidrijouda@gmail.com>
2023-12-02 05:48:54 +0100

    Setup jaeger opeator

--- src/main/kotlin/tern/artic/ArticController.kt
+++ src/main/kotlin/tern/artic/ArticController.kt
@@ -1,5 +1,6 @@
 package tern.artic
 
+import io.opencensus.trace.Tracing
 import org.slf4j.LoggerFactory
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PostMapping
@@ -12,8 +13,18 @@ class MessageResource(private val service: ArticService) {
     private val logger = LoggerFactory.getLogger(MessageResource::class.java)
     @GetMapping("/")
     fun find(): List<Message> {
-        logger.info("GET / - Retrieving messages")
-        return service.find()
+        logger.info("GET / - Retrieve messages")
+        val tracer = Tracing.getTracer()
+        val span = tracer.spanBuilder("ArticSpan")
+            .setRecordEvents(true)
+            .startSpan()
+
+        try {
+            logger.info("GET / - Artic span")
+            return service.find()
+        } finally {
+            span.end()
+        }
     }
 
     @PostMapping("/")
