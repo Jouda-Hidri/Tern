597f9c38f1a1b4c063d5d8798a98fbe41840b7d9
Jouda Hidri <hidrijouda@gmail.com>
2025-02-22 16:21:40 +0100

    Revert "Setup jaeger opeator"
    This reverts commit 99aabdc9e41a5f3ec6d96b662ab8167e12b994da.

--- src/main/kotlin/tern/artic/ArticController.kt
+++ src/main/kotlin/tern/artic/ArticController.kt
@@ -1,6 +1,5 @@
 package tern.artic
 
-import io.opencensus.trace.Tracing
 import org.slf4j.LoggerFactory
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PostMapping
@@ -13,18 +12,8 @@ class MessageResource(private val service: ArticService) {
     private val logger = LoggerFactory.getLogger(MessageResource::class.java)
     @GetMapping("/")
     fun find(): List<Message> {
-        logger.info("GET / - Retrieve messages")
-        val tracer = Tracing.getTracer()
-        val span = tracer.spanBuilder("ArticSpan")
-            .setRecordEvents(true)
-            .startSpan()
-
-        try {
-            logger.info("GET / - Artic span")
-            return service.find()
-        } finally {
-            span.end()
-        }
+        logger.info("GET / - Retrieving messages")
+        return service.find()
     }
 
     @PostMapping("/")
