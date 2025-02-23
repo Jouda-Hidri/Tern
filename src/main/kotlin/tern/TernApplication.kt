a6b3f565a2227324e0fc25690bf8891347d55ae4
Jouda Hidri <hidrijouda@gmail.com>
2025-02-23 14:29:43 +0100

    Implement CommandLineRunner using arguments

--- src/main/kotlin/tern/TernApplication.kt
+++ src/main/kotlin/tern/TernApplication.kt
@@ -1,11 +1,27 @@
 package tern
 
+import org.slf4j.LoggerFactory
+
+import org.springframework.boot.CommandLineRunner
+import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
-import org.springframework.boot.runApplication
 
 @SpringBootApplication
-class TernApplication
+class TernApplication : CommandLineRunner {
+    private val log = LoggerFactory.getLogger(TernApplication::class.java)
+
+    override fun run(vararg args: String?) {
+        log.info("run")
+        var sb = StringBuilder()
+        for (option in args) {
+            sb.append(" ").append(option)
+            log.info("OPTION: {}", option)
+        }
+        sb = if (sb.length == 0) sb.append("No Options Specified") else sb
+        log.info(String.format("WAR launched with following options: %s", sb.toString()))
+    }
+}
 
 fun main(args: Array<String>) {
-    runApplication<TernApplication>(*args)
+    SpringApplication.run(TernApplication::class.java, *args)
 }
