9ab7cb02cd437958884358f4d776015675949884
Jouda Hidri <hidrijouda@gmail.com>
2025-02-23 14:46:50 +0100

    Define args create and update for CommandLineRunner

--- src/main/kotlin/tern/TernApplication.kt
+++ src/main/kotlin/tern/TernApplication.kt
@@ -5,20 +5,28 @@ import org.slf4j.LoggerFactory
 import org.springframework.boot.CommandLineRunner
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import java.time.LocalDateTime
 
 @SpringBootApplication
 class TernApplication : CommandLineRunner {
     private val log = LoggerFactory.getLogger(TernApplication::class.java)
 
     override fun run(vararg args: String?) {
-        log.info("run")
-        var sb = StringBuilder()
-        for (option in args) {
-            sb.append(" ").append(option)
-            log.info("OPTION: {}", option)
+        when (args[0]) {
+            "create" -> {
+                val currentDate = LocalDateTime.now()
+                log.info("run job create at {}", currentDate)
+            }
+
+            "update" -> {
+                val currentDate = LocalDateTime.now()
+                log.info("run job update at {}", currentDate)
+            }
+
+            else -> {
+                log.error("Unknown job name {}", args[0])
+            }
         }
-        sb = if (sb.length == 0) sb.append("No Options Specified") else sb
-        log.info(String.format("WAR launched with following options: %s", sb.toString()))
     }
 }
 
