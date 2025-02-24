11c05322dfc63e8fcc085fcc134c65f8c2def489
Jouda Hidri <hidrijouda@gmail.com>
2025-02-24 22:02:02 +0100

    Add gRPC call to Antarctic from SpringBatch job

--- README.md
+++ README.md
@@ -1,8 +1,9 @@
 # Tern
 
 HTTP Artic -> gRPC Antarctic    
-K8s CronJob -> gRPC Antarctic   
-K8s CronJob -> SpringBatch
+K8s CronJob (grpcurl) -> gRPC Antarctic   
+K8s CronJob (job1) -> SpringBatch -> gRPC Antarctic    
+K8s CronJob (job2) -> SpringBatch    
 
 ## Setup
 
@@ -17,22 +18,19 @@ docker build -t tern .
 cd deployment    
 kubectl apply -f artic.yaml    
 kubectl apply -f antarctic.yaml  
-kubectl apply -f postgres.yaml  
 kubectl apply -f grpcurl-cronjob.yaml    
 kubectl apply -f job1-batch-cronjob.yaml    
 kubectl apply -f job2-batch-cronjob.yaml    
 minikube dashboard
 ````
-
-## SpringBatch
+## See results
 ````
-cd target/
-java -jar artic-0.0.1-SNAPSHOT.jar job1
+minikube service artic --url
 ````
 
 ## K8s CronJob
 ````
 kubectl get jobs --watch
-kubectl get pods --selector=job-name=job2-batch-cronjob-29007060
+kubectl get pods --selector=job1-batch-cronjob-29007149-rg7xt
 kubectl logs grpc-curl-job-29004415-mb5lb
 ````
--- src/main/kotlin/tern/jobs/BatchConfig.kt
+++ src/main/kotlin/tern/jobs/BatchConfig.kt
@@ -1,5 +1,8 @@
 package tern.jobs
 
+import com.google.protobuf.Empty
+import io.grpc.ManagedChannel
+import io.grpc.ManagedChannelBuilder
 import org.slf4j.LoggerFactory
 import org.springframework.batch.core.Job
 import org.springframework.batch.core.Step
@@ -9,12 +12,18 @@ import org.springframework.batch.core.configuration.annotation.StepBuilderFactor
 import org.springframework.batch.repeat.RepeatStatus
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import tern.grpc.TernServiceGrpc
 
 @Configuration
 @EnableBatchProcessing
 class BatchConfig {
 
     private val log = LoggerFactory.getLogger(BatchConfig::class.java)
+
+    private var channel: ManagedChannel = ManagedChannelBuilder.forTarget("antarctic.default.svc.cluster.local:30000")
+        .usePlaintext()
+        .build()
+    private var blockingStub = TernServiceGrpc.newBlockingStub(channel)
 
     @Bean
     fun job1(jobBuilderFactory: JobBuilderFactory, step1: Step): Job {
@@ -34,7 +43,11 @@ class BatchConfig {
     fun step1(stepBuilderFactory: StepBuilderFactory): Step {
         return stepBuilderFactory.get("step1")
             .tasklet { contribution, chunkContext ->
-                log.info("Executing job 1 step...")
+                log.info("Executing job 1")
+                val response = blockingStub.getMessage(Empty.getDefaultInstance())
+                response.forEachRemaining { message ->
+                    log.info("Batch response - $message")
+                }
                 RepeatStatus.FINISHED
             }
             .build()
